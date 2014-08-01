f04527539c3ff37423fed62537860191514cbba6
Yuki Anzai <anzai.y.aa@gmail.com>
2014-08-01 11:58:22 +0900

    faviconを追加

--- js/header.js
+++ js/header.js
@@ -10,13 +10,3 @@ document.addEventListener('core-media-change', function(e) {
   }
 });
 
-// custom transformation: scale header's title
-var titleStyle = document.querySelector('.logo').style;
-addEventListener('core-header-transform', function(e) {
-  var d = e.detail;
-  var m = d.height - d.condensedHeight;
-  var scale = Math.max(0.6, (m - d.y) / (m / 0.4) + 0.6);
-  titleStyle.paddingBottom = 18 + 72 * (scale - 0.6) / 0.4 + "px";
-  titleStyle.fontSize = 3 + 2 * (scale - 0.6) / 0.4 + "em";
-  titleStyle.paddingLeft = 10 + 15 * (scale - 0.6) / 0.4 + "px";
-});
