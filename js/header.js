9db590a28ba7731033eef2e131ccc48fb9aceceb
Yuki Anzai <anzai.y.aa@gmail.com>
2014-08-25 18:20:22 +0900

    モバイル対応

--- js/header.js
+++ js/header.js
@@ -1,7 +1,7 @@
 var output = document.querySelector('core-scroll-header-panel');
 document.addEventListener('core-media-change', function(e) {
   if(e.detail.matches) {
-    output.setAttribute("condensedHeaderHeight", "120px");
+    output.setAttribute("condensedHeaderHeight", "64px");
     output.setAttribute("keepCondensedHeader", "false");
   }
   else {
